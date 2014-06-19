5d1e40e7ae93601196cdd887d4f42951f24a6ac5
dogura <daiki.ogura@soliton.co.jp>
2014-06-19 14:55:27 +0900

    add setup infomation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+
 
 import setuptools
 
@@ -8,21 +9,22 @@ VERSION = '0.0.1'
 setuptools.setup(
     name='masala',
     version=VERSION,
-    packages=['pyexpression'],
-    install_requires=[],
+    packages=['masala'],
+    install_requires=['six'],
     author='OGURA_Daiki',
     author_email='8hachibee125@gmail.com',
     license='MIT',
-    url='',
-    description='curry',
+    url='https://github.com/hachibeeDI/masala',
+    description='curry functional patternpatch linq',
     long_description='',
     classifiers='''
 Programming Language :: Python
 Development Status :: 2 - Pre-Alpha
 License :: OSI Approved :: MIT License
 Programming Language :: Python :: 2
-Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Utilities
 '''.strip().splitlines())
